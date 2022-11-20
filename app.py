695a2ed851eb6ec1afddba3ee51d0b935aa7d6d0
jhoia <harrison.diaz@uptc.edu.co>
2022-11-19 20:40:07 -0500

    Cambios

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import hashlib
 import peewee
 from distutils.log import debug
-from flask import Flask, jsonify, request
+from playhouse.shortcuts import model_to_dict
+from flask import Flask, jsonify, request, redirect
 import os
 
 app = Flask(__name__)
@@ -12,87 +14,110 @@ cert_path = os.path.join(BASE_DIR, "DigiCertGlobalRootCA.crt.pem")
 conexion = peewee.MySQLDatabase(user="root", password="2zN3Oqnpi7ua2y9ewkeJ", host="containers-us-west-34.railway.app", port=7210, database="railway", ssl_ca=cert_path, ssl_disabled=False)
 
 
-class TipoDocumento(peewee.Model):
-    id_tipo_doc = peewee.PrimaryKeyField(11)
-    nombre_tipo = peewee.CharField(30)
+#orm
+class Materia(peewee.Model):
+    cod_materia = peewee.PrimaryKeyField()
+    nombre_materia = peewee.CharField(70)
+    creditos = peewee.IntegerField(1)
+    cupos = peewee.IntegerField(3)
+    estado_materia = peewee.BooleanField()
+
     class Meta:
         database = conexion
-        db_table = "TIPO_DOCUMENTO"
+        db_table = "MATERIA"
 
-class Estudiante(peewee.Model):
-    cod_estudiante = peewee.PrimaryKeyField(11)
-    tipo_documento = peewee.IntegerField(11)
-    nombre_estudiante = peewee.CharField(30)
-    apellido_estudiante = peewee.CharField(30)
-    foto = peewee.CharField(150)
-    estado = peewee.CharField(1)
-    class Meta:
-        database = conexion
-        db_table = "ESTUDIANTE"
+# Materias
 
-
-@app.route("/estudiante")
-def estudiante():
+@app.route("/materia")
+def materia():
     return jsonify({'mensaje' : "Usuario logueado exitosamente!"})
 
 
 
-@app.route("/estudiante/listar")
-def listarEstudiantes():
+@app.route("/materia/listar")
+def listarMaterias():
+    items = Materia.select()
+    items = [model_to_dict(item) for item in items]
 
-    estudiantes = Estudiante.select(Estudiante, TipoDocumento).join(TipoDocumento, attr = 'tip', on=(Estudiante.tipo_documento == TipoDocumento.id_tipo_doc)).execute()
+    return jsonify(items)
 
-    str = [{'cod_estudiante': estudiante.cod_estudiante,'tipo_documento' : estudiante.tip.nombre_tipo,'nombre_estudiante' : estudiante.nombre_estudiante,'apellido_estudiante' : estudiante.apellido_estudiante,'foto' : estudiante.foto,'estado' : estudiante.estado} for estudiante in estudiantes]
+@app.route("/materia/registrar", methods=["POST"])
+def registrarMateria(): 
 
-    return jsonify(str)
+    cod = request.json['cod_materia']
+    nom, cre, cup, status = getInfo()
 
-@app.route("/estudiante/registrar", methods=["POST"])
-def registrarEstudiante(): 
-    id_est = request.json['cod_estudiante']
-    tipo_doc, nom_est, ape_est,foto,esta = getInfo()
+    Materia.create(cod_materia = cod, nombre_materia = nom, creditos = cre, cupos = cup, estado_materia = status)
 
-    Estudiante.create(cod_estudiante = id_est, tipo_documento = tipo_doc, nombre_estudiante = nom_est, apellido_estudiante =ape_est, foto = foto, estado = esta)
-
-    return jsonify({'mensaje' : "Estudiante registrado"})
+    return jsonify({'mensaje' : "Materia registrada"})
 
 def getInfo():
-    tipo_doc = request.json['tipo_documento']
-    nom_est = request.json['nombre_estudiante']
-    ape_est = request.json['apellido_estudiante']
-    foto = request.json['foto']
-    esta = request.json['estado']
-    return tipo_doc,nom_est,ape_est,foto,esta
+    nom = request.json['nombre_materia']
+    cre = request.json['creditos']
+    cup = request.json['cupos']
+    status = request.json['estado_materia']
+    return nom,cre,cup,status
 
-@app.route('/estudiante/actualizar/<codigo>', methods=["PUT"])
-def actualizarEstudiante(codigo):
+@app.route('/materia/actualizar/<codigo>', methods=["PUT"])
+def actualizarMateria(codigo):
     
-    tipo_doc, nom_est, ape_est,foto,esta = getInfo()
+    nom, cre, cup, status = getInfo()
 
-    Estudiante.update(tipo_documento = tipo_doc, nombre_estudiante = nom_est, apellido_estudiante =ape_est, foto = foto, estado = esta).where(Estudiante.cod_estudiante == codigo).execute()
+    Materia.update(nombre_materia = nom, creditos = cre, cupos = cup, estado_materia = status).where(Materia.cod_materia == codigo).execute()
     
-    return jsonify({'mensaje' : "Estudiante actualizado"})
+    return jsonify({'mensaje' : "Materia actualizado"})
 
-@app.route('/estudiante/actualizar/estado/<codigo>', methods=["PATCH"])
-def actualizarEstudianteE(codigo):
+@app.route('/materia/eliminar/<codigo>', methods=["DELETE"])
+def eliminarMateria(codigo):
+
+    Materia.delete().where(Materia.cod_materia == codigo).execute()
+
+    return jsonify({'mensaje' : "Materia eliminada"})
     
-    esta = request.json['estado']
+#orm usuarios
+class Usuario(peewee.Model):
+    id_usuario = peewee.PrimaryKeyField()
+    nombre_usuario = peewee.CharField(40)
+    password = peewee.CharField(200)
 
-    Estudiante.update(estado = esta).where(Estudiante.cod_estudiante == codigo).execute()
-    
-    return jsonify({'mensaje' : "Estado del Estudiante actualizado"})
+    class Meta:
+        database = conexion
+        db_table = "USUARIO"
 
 
-@app.route('/estudiante/eliminar/<codigo>', methods=["DELETE"])
-def eliminarEstudiante(codigo):
+@app.route("/registrar", methods=["POST"])
+def registrarUsuario():
+
+    id_usuario = request.json['id_usuario']
+    nombre_usuario = request.json['nombre_usuario']
+    password = request.json['password']
+    #encriptando
+    enc = hashlib.sha256(password.encode())
+    pass_enc = enc.hexdigest()
+
+    Usuario.create(id_usuario = id_usuario, nombre_usuario=nombre_usuario, password = pass_enc)
+
+    return jsonify({'mensaje' : "Usuario registrado"})
+
+@app.route("/login", methods=["POST"])
+def loginUsuario():
+    nombre_usuario = request.json['nombre_usuario']
+    password = request.json['password']
+    #encriptando
+    enc = hashlib.sha256(password.encode())
+    pass_enc = enc.hexdigest()
 
     try:
-        Estudiante.delete().where(Estudiante.cod_estudiante == codigo).execute()
+        user = Usuario.select().where(Usuario.nombre_usuario == nombre_usuario and Usuario.password == pass_enc)
+        Item = [model_to_dict(item) for item in user]
     except:
-        Estudiante.update(estado = 'E').where(Estudiante.cod_estudiante == codigo).execute()
+        user = ""
+    if(Item!=[]):
+        return jsonify({'msg': "Usuario fue encontrado con exito"})
+    else:
+        return jsonify({'msg': "El usuario no existe "})
 
-    return jsonify({'mensaje' : "Estudiante eliminado"})
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
-
+    app.run(debug=True,host="0.0.0.0")
