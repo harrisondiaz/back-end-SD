529d36b5c9b6420c5f26ce73cb9d712e32d26654
jhoia <harrison.diaz@uptc.edu.co>
2022-11-19 21:42:25 -0500

    Cambios

--- app.py
+++ app.py
@@ -118,6 +118,94 @@ def loginUsuario():
         return jsonify({'msg': "El usuario no existe "})
 
 
+class TipoDocumento(peewee.Model):
+    id_tipo_doc = peewee.PrimaryKeyField(11)
+    nombre_tipo = peewee.CharField(30)
+
+    class Meta:
+        database = conexion
+        db_table = "TIPO_DOCUMENTO"
+
+
+class Estudiante(peewee.Model):
+    id_estudiante = peewee.PrimaryKeyField(11)
+    tipo_documento = peewee.IntegerField(11)
+    nombre_estudiante = peewee.CharField(30)
+    apellido_estudiante = peewee.CharField(30)
+    foto = peewee.CharField(150)
+    estado = peewee.CharField(1)
+
+    class Meta:
+        database = conexion
+        db_table = "Estudiante"
+
+
+@app.route("/estudiante")
+def estudiante():
+    return jsonify({'mensaje': "Usuario logueado exitosamente!"})
+
+
+@app.route("/estudiante/listar")
+def listarEstudiantes():
+    estudiantes = Estudiante.select(Estudiante, TipoDocumento).join(TipoDocumento, attr='tip', on=(
+                Estudiante.tipo_documento == TipoDocumento.id_tipo_doc)).execute()
+
+    str = [{'id_estudiante': estudiante.id_estudiante, 'tipo_documento': estudiante.tip.nombre_tipo,
+            'nombre_estudiante': estudiante.nombre_estudiante, 'apellido_estudiante': estudiante.apellido_estudiante,
+            'foto': estudiante.foto, 'estado': estudiante.estado} for estudiante in estudiantes]
+
+
+    return jsonify(str)
+
+
+@app.route("/estudiante/registrar", methods=["POST"])
+def registrarEstudiante():
+    id_est = request.json['cod_estudiante']
+    tipo_doc, nom_est, ape_est, foto, esta = getInfo()
+
+    Estudiante.create(cod_estudiante=id_est, tipo_documento=tipo_doc, nombre_estudiante=nom_est,
+                      apellido_estudiante=ape_est, foto=foto, estado=esta)
+
+    return jsonify({'mensaje': "Estudiante registrado"})
+
+
+def getInfo():
+    tipo_doc = request.json['tipo_documento']
+    nom_est = request.json['nombre_estudiante']
+    ape_est = request.json['apellido_estudiante']
+    foto = request.json['foto']
+    esta = request.json['estado']
+    return tipo_doc, nom_est, ape_est, foto, esta
+
+
+@app.route('/estudiante/actualizar/<codigo>', methods=["PUT"])
+def actualizarEstudiante(codigo):
+    tipo_doc, nom_est, ape_est, foto, esta = getInfo()
+
+    Estudiante.update(tipo_documento=tipo_doc, nombre_estudiante=nom_est, apellido_estudiante=ape_est, foto=foto,
+                      estado=esta).where(Estudiante.cod_estudiante == codigo).execute()
+
+    return jsonify({'mensaje': "Estudiante actualizado"})
+
+
+@app.route('/estudiante/actualizar/estado/<codigo>', methods=["PATCH"])
+def actualizarEstudianteE(codigo):
+    esta = request.json['estado']
+
+    Estudiante.update(estado=esta).where(Estudiante.cod_estudiante == codigo).execute()
+
+    return jsonify({'mensaje': "Estado del Estudiante actualizado"})
+
+
+@app.route('/estudiante/eliminar/<codigo>', methods=["DELETE"])
+def eliminarEstudiante(codigo):
+    try:
+        Estudiante.delete().where(Estudiante.cod_estudiante == codigo).execute()
+    except:
+        Estudiante.update(estado='E').where(Estudiante.cod_estudiante == codigo).execute()
+
+    return jsonify({'mensaje': "Estudiante eliminado"})
+
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
