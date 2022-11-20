459e49402277b8a5d88c209225b6e6833f6e9130
jhoia <harrison.diaz@uptc.edu.co>
2022-11-19 23:23:56 -0500

    Cambios

--- app.py
+++ app.py
@@ -3,21 +3,23 @@ import peewee
 from distutils.log import debug
 from playhouse.shortcuts import model_to_dict
 from flask import Flask, jsonify, request, redirect
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 import os
 
 app = Flask(__name__)
+CORS(app)
+
 app.secret_key = os.urandom(24)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 cert_path = os.path.join(BASE_DIR, "DigiCertGlobalRootCA.crt.pem")
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 #conexion con Azure
 conexion = peewee.MySQLDatabase(user="root", password="2zN3Oqnpi7ua2y9ewkeJ", host="containers-us-west-34.railway.app", port=7210, database="railway")
 
 
 #orm
 class Materia(peewee.Model):
-    cod_materia = peewee.PrimaryKeyField()
+    id_materia = peewee.PrimaryKeyField()
+    cod_materia = peewee.IntegerField(3)
     nombre_materia = peewee.CharField(70)
     creditos = peewee.IntegerField(1)
     cupos = peewee.IntegerField(3)
@@ -28,46 +30,47 @@ class Materia(peewee.Model):
         db_table = "Materia"
 
 # Materias
-
+@cross_origin
 @app.route("/materia")
 def materia():
     return jsonify({'mensaje' : "Usuario logueado exitosamente!"})
 
 
-
+@cross_origin
 @app.route("/materia/listar")
 def listarMaterias():
     items = Materia.select()
     items = [model_to_dict(item) for item in items]
 
     return jsonify(items)
-
+@cross_origin()
 @app.route("/materia/registrar", methods=["POST"])
-def registrarMateria(): 
+def registrarMateria():
+    id_materia = request.json['id_materia']
+    cod, nom, cre, cup, status = getInfoAssignature()
 
-    cod = request.json['cod_materia']
-    nom, cre, cup, status = getInfo()
-
-    Materia.create(cod_materia = cod, nombre_materia = nom, creditos = cre, cupos = cup, estado_materia = status)
+    Materia.create(id_materia = id_materia,cod_materia = cod, nombre_materia = nom, creditos = cre, cupos = cup, estado_materia = status)
 
     return jsonify({'mensaje' : "Materia registrada"})
 
-def getInfo():
+def getInfoAssignature():
+
+    cod = request.json['cod_materia']
     nom = request.json['nombre_materia']
     cre = request.json['creditos']
     cup = request.json['cupos']
     status = request.json['estado_materia']
-    return nom,cre,cup,status
-
+    return cod,nom,cre,cup,status
+@cross_origin
 @app.route('/materia/actualizar/<codigo>', methods=["PUT"])
 def actualizarMateria(codigo):
     
-    nom, cre, cup, status = getInfo()
+    nom, cre, cup, status = getInfoAssignature()
 
     Materia.update(nombre_materia = nom, creditos = cre, cupos = cup, estado_materia = status).where(Materia.cod_materia == codigo).execute()
     
     return jsonify({'mensaje' : "Materia actualizado"})
-
+@cross_origin
 @app.route('/materia/eliminar/<codigo>', methods=["DELETE"])
 def eliminarMateria(codigo):
 
@@ -85,7 +88,7 @@ class Usuario(peewee.Model):
         database = conexion
         db_table = "Usuario"
 
-
+@cross_origin
 @app.route("/registrar", methods=["POST"])
 def registrarUsuario():
 
@@ -99,7 +102,7 @@ def registrarUsuario():
     Usuario.create(id_usuario = id_usuario, nombre_usuario=nombre_usuario, password = pass_enc)
 
     return jsonify({'mensaje' : "Usuario registrado"})
-
+@cross_origin
 @app.route("/login", methods=["POST"])
 def loginUsuario():
     nombre_usuario = request.json['nombre_usuario']
@@ -140,12 +143,12 @@ class Estudiante(peewee.Model):
         database = conexion
         db_table = "Estudiante"
 
-
+@cross_origin
 @app.route("/estudiante")
 def estudiante():
     return jsonify({'mensaje': "Usuario logueado exitosamente!"})
 
-
+@cross_origin
 @app.route("/estudiante/listar")
 def listarEstudiantes():
     estudiantes = Estudiante.select(Estudiante, TipoDocumento).join(TipoDocumento, attr='tip', on=(
@@ -158,11 +161,11 @@ def listarEstudiantes():
 
     return jsonify(str)
 
-
+@cross_origin
 @app.route("/estudiante/registrar", methods=["POST"])
 def registrarEstudiante():
     id_est = request.json['cod_estudiante']
-    tipo_doc, nom_est, ape_est, foto, esta = getInfo()
+    tipo_doc, nom_est, ape_est, foto, esta = getInfoStudient()
 
     Estudiante.create(cod_estudiante=id_est, tipo_documento=tipo_doc, nombre_estudiante=nom_est,
                       apellido_estudiante=ape_est, foto=foto, estado=esta)
@@ -170,7 +173,7 @@ def registrarEstudiante():
     return jsonify({'mensaje': "Estudiante registrado"})
 
 
-def getInfo():
+def getInfoStudient():
     tipo_doc = request.json['tipo_documento']
     nom_est = request.json['nombre_estudiante']
     ape_est = request.json['apellido_estudiante']
@@ -178,17 +181,17 @@ def getInfo():
     esta = request.json['estado']
     return tipo_doc, nom_est, ape_est, foto, esta
 
-
+@cross_origin
 @app.route('/estudiante/actualizar/<codigo>', methods=["PUT"])
 def actualizarEstudiante(codigo):
-    tipo_doc, nom_est, ape_est, foto, esta = getInfo()
+    tipo_doc, nom_est, ape_est, foto, esta = getInfoStudient()
 
     Estudiante.update(tipo_documento=tipo_doc, nombre_estudiante=nom_est, apellido_estudiante=ape_est, foto=foto,
                       estado=esta).where(Estudiante.cod_estudiante == codigo).execute()
 
     return jsonify({'mensaje': "Estudiante actualizado"})
 
-
+@cross_origin
 @app.route('/estudiante/actualizar/estado/<codigo>', methods=["PATCH"])
 def actualizarEstudianteE(codigo):
     esta = request.json['estado']
@@ -197,7 +200,7 @@ def actualizarEstudianteE(codigo):
 
     return jsonify({'mensaje': "Estado del Estudiante actualizado"})
 
-
+@cross_origin
 @app.route('/estudiante/eliminar/<codigo>', methods=["DELETE"])
 def eliminarEstudiante(codigo):
     try:
